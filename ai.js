18346518d0b05fab59b259742e78c84508999c92
Daniel Cruz <dcruz2@oberlin.edu>
2023-04-08 20:38:59 -0400

    Uploaded files for game AI

---
+++ ai.js
@@ -0,0 +1,10 @@
+// This file contains the functions that determine the actions of the AI
+
+function getOpponentAction() {
+    // get opponent's action
+    return opp.action; 
+}
+
+function move(){
+    // determines the CPU's actions
+}
