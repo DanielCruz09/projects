a9b72390abd239d3e63396c117c4311ed7897147
Daniel Cruz <dcruz2@oberlin.edu>
2023-05-08 14:22:03 -0400

    Fixed some of the functions; will implement move function soon

--- ai.js
+++ ai.js
@@ -1,7 +1,7 @@
 // // This file contains the functions that determine the actions of the AI
 
 checker.prototype.isEmpty = function(x, y) {
-    let pos = currBord[x][y];
+    let pos = currBord[x][y].piece;
     if (pos == null) return true;
     return false;
 }
@@ -13,82 +13,21 @@ checker.prototype.capture() = function(oldx, oldy, newx, newy) {
     target = null;
 }
 
-checker.prototype.getAllowedMoves = function () {
-        let board = currBord;
-        const moves = {}; // key = position of a piece, value = array of possible positions for that piece
-        for (let i=0; i<8; i++) {
-            for (let j=0; j<8; j++) {
-                var southeast = board[i+1][j+1];
-                var southwest = board[i-1][j+1];
-                var northwest = board[i-1][j-1];
-                var northeast = board[i+1][j-1];
-                var directions = [];
-                if (this.color == "white" & this.isEmpty(i+1,j+1) & this.isEmpty(i-1,j+1) & this.isEmpty(i-1,j-1) & this.isEmpty(i+1,j-1)) {
-                    if (i == 0) {
-                        directions.push(southeast);
-                        if (j == 0) {
-                            moves.board[i][j] = directions;
-                            continue;
-                        }
-                        directions.push(southwest);
-                        moves.board[i][j] = directions;
-                    }
-                    else if (i == 7) {
-                        directions.push(southwest);
-                        if (j == 0) {
-                            moves.board[i][j] = directions;
-                            continue;
-                        }
-                        directions.push(northwest);
-                        moves.board[i][j] = directions;
-                    }
-                    else if (j == 7) {
-                        directions.push(northeast);
-                        
-                        if (i == 0) {
-                            moves.board[i][j] = directions;
-                            continue;
-                        }
-                        directions.push(northwest);
-                        moves.board[i][j] = directions;
-                    }
-                    else if (i == 7 & j == 7) {
-                        directions.push(northwest);
-                        moves.board[i][j] = directions;
-                    }
-                    else {
-                        directions.push(northwest);
-                        directions.push(northeast);
-                        directions.push(southwest);
-                        directions.push(southeast);
-                        moves.board[i][j] = directions;
-                    }
-                }
-                var randNum = Math.floor(Math.random() * 100);
-                if (this.color == "white" & !this.isEmpty(i+1,j+1)) {
-                    // Capture is possible, move to the square diagonally
-                    // only capture if the random number is greater than 50
-                    if (randNum > 50) this.capture(i+1,j+1,i+2,j+2);
-                }
-                if (this.color == "white" & !this.isEmpty(i-1,j+1)) {
-                    if (randNum > 50) this.capture(i-1,j+1,i-2,j+2);
-                }
-                if (this.color == "white" & !this.isEmpty(i-1,j-1)) {
-                    if (randNum > 50) this.capture(i-1,j-1,i-2,j-2);
-                }
-                if (this.color == "white" & !this.isEmpty(i+1,j-1)) {
-                    if (randNum > 50) this.capture(i+1,j-1,i+2,j-2);
-                }
-            }
+checker.prototype.getAllowedMoves = function (x,y) {
+        var board = currBord;
+        var southeast = board[x+1][y+1];
+        var southwest = board[x-1][y+1];
+        var directions = [];
+        if (this.color == "white") {
+            if (this.isEmpty(x+1,y+1)) directions.push(southeast);
+            if (this.isEmpty(x-1,y+1)) directions.push(southwest);
         }
-        return moves;
+        return directions;
     }
 
 function init() {
     for (var i = 1; i <= 64; i++)
         block[i] = new square_p(square_class[i], i);
-
-
 
     for (var i = 1; i <= 4; i++) {
         w_checker[i] = new checker(white_checker_class[i], "white", 2 * i - 1);
@@ -137,7 +76,78 @@ function init() {
 
 function move() {
     init();
-    var nextMoves = getAllowedMoves();
-    var randInt = Math.floor(Math.random() * 64);
-    return nextMoves;
+    var nextMoves = [];
+    for (let i=0; i<64; i++) {
+        for (let j=0; j<64; j++) {
+            // TODO
+        }
+    }
 }
+
+// Alternate implementation for getAllowedMoves:
+// const moves = {}; // key = position of a piece, value = array of possible positions for that piece
+        // for (let i=0; i<8; i++) {
+        //     for (let j=0; j<8; j++) {
+        //         var southeast = board[i+1][j+1];
+        //         var southwest = board[i-1][j+1];
+        //         var northwest = board[i-1][j-1];
+        //         var northeast = board[i+1][j-1];
+        //         var directions = [];
+        //         if (this.color == "white" & this.isEmpty(i+1,j+1) & this.isEmpty(i-1,j+1) & this.isEmpty(i-1,j-1) & this.isEmpty(i+1,j-1)) {
+        //             if (i == 0) {
+        //                 directions.push(southeast);
+        //                 if (j == 0) {
+        //                     moves.board[i][j] = directions;
+        //                     continue;
+        //                 }
+        //                 directions.push(southwest);
+        //                 moves.board[i][j] = directions;
+        //             }
+        //             else if (i == 7) {
+        //                 directions.push(southwest);
+        //                 if (j == 0) {
+        //                     moves.board[i][j] = directions;
+        //                     continue;
+        //                 }
+        //                 directions.push(northwest);
+        //                 moves.board[i][j] = directions;
+        //             }
+        //             else if (j == 7) {
+        //                 directions.push(northeast);
+                        
+        //                 if (i == 0) {
+        //                     moves.board[i][j] = directions;
+        //                     continue;
+        //                 }
+        //                 directions.push(northwest);
+        //                 moves.board[i][j] = directions;
+        //             }
+        //             else if (i == 7 & j == 7) {
+        //                 directions.push(northwest);
+        //                 moves.board[i][j] = directions;
+        //             }
+        //             else {
+        //                 directions.push(northwest);
+        //                 directions.push(northeast);
+        //                 directions.push(southwest);
+        //                 directions.push(southeast);
+        //                 moves.board[i][j] = directions;
+        //             }
+        //         }
+        //         var randNum = Math.floor(Math.random() * 100);
+        //         if (this.color == "white" & !this.isEmpty(i+1,j+1)) {
+        //             // Capture is possible, move to the square diagonally
+        //             // only capture if the random number is greater than 50
+        //             if (randNum > 50) this.capture(i+1,j+1,i+2,j+2);
+        //         }
+        //         if (this.color == "white" & !this.isEmpty(i-1,j+1)) {
+        //             if (randNum > 50) this.capture(i-1,j+1,i-2,j+2);
+        //         }
+        //         if (this.color == "white" & !this.isEmpty(i-1,j-1)) {
+        //             if (randNum > 50) this.capture(i-1,j-1,i-2,j-2);
+        //         }
+        //         if (this.color == "white" & !this.isEmpty(i+1,j-1)) {
+        //             if (randNum > 50) this.capture(i+1,j-1,i+2,j-2);
+        //         }
+        //     }
+        // }
